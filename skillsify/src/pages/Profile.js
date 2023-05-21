9cfe6ba3b17c85427a2c42bd84c2a773d2a41023
jovanaivanovska11 <jovanaivanovska11@yahoo.com>
2023-05-21 19:06:27 +0200

    changed profile page

--- skillsify/src/pages/Profile.js
+++ skillsify/src/pages/Profile.js
@@ -43,48 +43,50 @@ export default function Profile() {
                 </Link>
                 <div class="bg-gray-100 h-10 w-10"></div>
             </div>
-            <div class="flex items-center mt-96 ml-20">
+            <div class="flex items-center mt-72 ml-20">
                 <CogIcon className="h-16 w-16 text-darkgraycustom" />
                 <p className="text-darkgraycustom text-lg mr-4 ml-6">Settings</p>
                 <div class="bg-gray-100 h-10 w-10"></div>
             </div>
-            <div class="container mx-auto py-10 -ml-20" style={{marginTop:"-680px" }}>
+            <div class="container mx-auto py-10 -ml-20" style={{marginTop:"-610px" }}>
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                     <div class="hidden md:block md:col-span-1"></div>
                         <div class="col-span-1 md:col-span-2 bg-white rounded-lg shadow-lg p-6">
                             <h2 class="text-2xl font-poppins font-bold text-left text-bluecustom ml-10 mt-4">Personal Details</h2>
                             <h2 class="text-2xl font-poppins font-bold text-right text-bluecustom mr-80 -mt-8">Courses</h2>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-2">Name</h3>
-                            <h3 class="font-poppins font-bold text-right text-gray -mt-4" style={{marginRight:"150px"}}>
-                                <span class="border p-2 border-white rounded-full bg-lightgraycustom">Active<span class="ml-32 text-sm border border-black rounded-full p-1">2 courses</span></span>
+                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-4">Name</h3>
+                            <h3 class="font-poppins font-bold text-right text-gray mt-2" style={{marginRight:"20px"}}>
+                                <span class="border p-32 border-white rounded-lg bg-lightgraycustom text-sm" style={{paddingTop:"30px"}}>
+                                    Active
+                                    <span class="ml-32 text-sm border border-black rounded-full p-1 text-xs">3 courses</span>
+                                </span>
                             </h3>
-                            <button className="block font-oswald text-right text-bluecustom text-sm mt-2" style={{marginLeft:"730px"}}>View Courses</button>
-                            <h3 class="font-poppins font-bold text-left text-gray ml-12 -mt-6">Jane Dpe</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12  mt-4">Gender</h3>
-                            <h3 class="font-poppins font-bold text-right text-gray -mt-4" style={{marginRight:"135px"}}>
-                                <span class="border p-2 border-white rounded-full bg-lightgraycustom">Finished<span class="ml-32 text-sm border border-black rounded-full p-1">2 courses</span></span>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 1 ........................................................................</p>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 2 .......................................................................</p>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 3 ........................................................................</p>
+                            <h3 class="font-poppins font-bold text-left text-gray ml-12 -mt-32">Jane Dpe</h3>
+                            <button class="font-poppins text-bluecustom text-xs" style={{marginLeft:'860px', marginTop:"100px"}}>View more</button>
+                            <h3 class="font-poppins font-bold text-right text-gray mt-16" style={{marginRight:"10px"}}>
+                                <span class="border p-32 border-white rounded-lg bg-lightgraycustom text-sm" style={{paddingTop:"30px"}}>
+                                    Finished
+                                    <span class="ml-32 text-sm border border-black rounded-full p-1 text-xs">5 courses</span>
+                                </span>
                             </h3>
-                            <button className="block font-oswald text-right text-bluecustom text-sm mt-2" style={{marginLeft:"750px"}}>View Courses</button>
-                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  -mt-6">Female</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-4">Date of Birth</h3>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 1 ........................................................................</p>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 2 .......................................................................</p>
+                            <p className="font-poppins text-sm font-bold mt-4" style={{marginLeft:"550px"}}>Course 3 ........................................................................</p>
+                            <button class="font-poppins text-bluecustom text-xs" style={{marginLeft:'860px'}}>View more</button>
+                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12" style={{marginTop:"-320px"}}>Date of Birth</h3>
                             <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">August 27th, 1999</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12  mt-4">Nationality</h3>
-                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">French</h3>
-                            <h2 class="text-2xl font-poppins font-bold text-left text-bluecustom ml-10 mt-4">Address</h2>
-                            <h2 class="text-2xl font-poppins font-bold text-right text-bluecustom mr-80 -mt-8">Contact</h2>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12  mt-4">Address Line</h3>
-                            <h3 class="font-poppins font-bold text-right text-darkgraycustom mr-72 -mt-6">Phone Number</h3>
+                            <h2 class="text-2xl font-poppins font-bold text-left text-bluecustom ml-10 mt-8">Address</h2>
                             <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">No 35 Jimmy Ebi Street</h3>
-                            <h3 class="font-poppins font-bold text-right text-gray mr-72 -mt-6" style={{marginRight:"297px" }}>09034867656</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12  mt-4">City</h3>
-                            <h3 class="font-poppins font-bold text-right text-darkgraycustom -mt-6" style={{marginRight:"360px" }}>Email</h3>
-                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">Value</h3>
-                            <h3 class="font-poppins font-bold text-right text-gray mr-72 -mt-6" style={{marginRight:"255px" }}>tomilola@me.com</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12  mt-4">State</h3>
-                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">Value</h3>
-                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-4">Country</h3>
-                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">Value</h3>
-                            <button className="inline-block rounded bg-bluecustom px-6 pb-2 pt-2 text-bold font-medium text-white" style={{marginLeft:"850px" }}>Edit</button>
+                            <h2 class="text-2xl font-poppins font-bold text-left text-bluecustom ml-10 mt-8">Contact</h2>
+                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-2">Phone Number</h3>
+                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">09034867656</h3>
+                            <h3 class="font-poppins font-bold text-left text-darkgraycustom ml-12 mt-2">Email</h3>
+                            <h3 class="font-poppins font-bold text-left  text-gray ml-12  mt-2">tomilola@me.com</h3>
+                            <button class="font-poppins rounded-full border border-lightgraycustom bg-lightgraycustom px-4 py-2 text-bluecustom ml-10 mt-10">Edit</button>
+                            <button class="font-poppins rounded-full border border-lightgraycustom bg-lightgraycustom px-4 py-2 text-bluecustom ml-4">Change password</button>
                         </div>
                 </div>
             </div>
